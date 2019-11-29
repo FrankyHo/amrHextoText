513179e95c6a4c9dcb7efe19f9929a1688f0f97f
Franky Ho <franky@sentronic.hk>
2019-11-29 11:02:56 +0800

    require buffer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict'
+const Buffer = require('buffer').Buffer
 const convert = (from, to) => str => Buffer.from(str, from).toString(to)
 exports.textToHex = convert('utf8', 'hex')
 exports.hexToUtf8 = convert('hex', 'utf8')
